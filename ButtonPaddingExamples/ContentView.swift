58c453568d64143f89da25f89e51309c189cd216
Russell Gordon <rgordon@lcs.on.ca>
2021-11-29 06:49:15 -0500

    A few examples of how to style buttons.

--- ButtonPaddingExamples/ContentView.swift
+++ ButtonPaddingExamples/ContentView.swift
@@ -9,8 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+
+            // No styling
+            Button(action: {
+                print("Button 1 was pressed")
+            }, label: {
+                Text("Button 1")
+            })
+            .buttonStyle(.bordered)
+
+            // Larger text
+            Button(action: {
+                print("Button 2 was pressed")
+            }, label: {
+                Text("Button 2")
+                    .font(.largeTitle)
+            })
+            .buttonStyle(.bordered)
+
+            // Regular text, with padding
+            Button(action: {
+                print("Button 3 was pressed")
+            }, label: {
+                Text("Button 3")
+                    .padding()
+            })
+            .buttonStyle(.bordered)
+
+            // Large text, bolded, with padding, and a tint
+            Button(action: {
+                print("Button 3 was pressed")
+            }, label: {
+                Text("Button 3")
+                    .bold()
+                    .font(.largeTitle)
+                    .padding()
+            })
+            .buttonStyle(.bordered)
+            .tint(.purple)
+
+        }
     }
 }
 
