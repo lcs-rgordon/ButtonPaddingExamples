f8c146433f83da1ac42d1090b580243184a4e205
Russell Gordon <rgordon@lcs.on.ca>
2021-11-29 06:39:03 -0500

    Initial Commit

---
+++ ButtonPaddingExamples/ButtonPaddingExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonPaddingExamplesApp.swift
+//  ButtonPaddingExamples
+//
+//  Created by Russell Gordon on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct ButtonPaddingExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
